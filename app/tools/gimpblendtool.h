DemonRem/gimp

app: implement blend tool gui for gradient stops
Allows setting the stop's position, and its left and right colors
and color types.  A chain button can be used to modify the two
colors (and color types) together.

The stop can be deleted through an editor button.

app/tools/gimpblendtool.h
@@ -71,6 +71,13 @@ struct _GimpBlendTool
   GtkWidget          *endpoint_se;
   GtkWidget          *endpoint_color_panel;
   GtkWidget          *endpoint_type_combo;
+  GtkWidget          *stop_editor;
+  GtkWidget          *stop_spinbutton;
+  GtkWidget          *stop_left_color_panel;
+  GtkWidget          *stop_left_type_combo;
+  GtkWidget          *stop_right_color_panel;
+  GtkWidget          *stop_right_type_combo;
+  GtkWidget          *stop_chain_button;
 };
 
 struct _GimpBlendToolClass

unascribed-forks/Glimpse

app/tools/gimpblendtool.h
@@ -72,6 +72,13 @@ struct _GimpBlendTool
   GtkWidget          *endpoint_se;
   GtkWidget          *endpoint_color_panel;
   GtkWidget          *endpoint_type_combo;
+  GtkWidget          *stop_editor;
+  GtkWidget          *stop_se;
+  GtkWidget          *stop_left_color_panel;
+  GtkWidget          *stop_left_type_combo;
+  GtkWidget          *stop_right_color_panel;
+  GtkWidget          *stop_right_type_combo;
+  GtkWidget          *stop_chain_button;
 };
 
 struct _GimpBlendToolClass
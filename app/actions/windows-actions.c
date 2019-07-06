GNOME/gimp

app: Add option in the windows menu to hide the image tab bar.

app/actions/windows-actions.c
@@ -126,6 +126,13 @@ static const GimpToggleActionEntry windows_toggle_actions[] =
     FALSE,
     GIMP_HELP_WINDOWS_HIDE_DOCKS },
 
+  { "windows-hide-tabs", NULL,
+    NC_("windows-action", "Hide Tabs"), NULL,
+    NC_("windows-action", "When enabled, the image tab bar is hidden."),
+    G_CALLBACK (windows_hide_tabs_cmd_callback),
+    FALSE,
+    GIMP_HELP_WINDOWS_SHOW_TABS },
+
   { "windows-use-single-window-mode", NULL,
     NC_("windows-action", "Single-Window Mode"), NULL,
     NC_("windows-action", "When enabled, GIMP is in a single-window mode."),
@@ -249,6 +256,7 @@ windows_actions_update (GimpActionGroup *group,
 
   SET_ACTIVE ("windows-use-single-window-mode", config->single_window_mode);
   SET_ACTIVE ("windows-hide-docks", config->hide_docks);
+  SET_ACTIVE ("windows-hide-tabs", config->hide_tabs);
 
   switch (config->tabs_position)
     {
@@ -271,6 +279,7 @@ windows_actions_update (GimpActionGroup *group,
 
   gimp_action_group_set_action_active (group, action, TRUE);
   gimp_action_group_set_action_sensitive (group, "windows-tab-position", config->single_window_mode);
+  gimp_action_group_set_action_sensitive (group, "windows-hide-tabs", config->single_window_mode);
 
 #undef SET_ACTIVE
 }

app/actions/windows-commands.c
@@ -66,6 +66,23 @@ windows_hide_docks_cmd_callback (GtkAction *action,
 }
 
 void
+windows_hide_tabs_cmd_callback (GtkAction *action,
+                                gpointer data)
+{
+  Gimp     *gimp;
+  gboolean  active;
+  return_if_no_gimp (gimp, data);
+
+  active = gtk_toggle_action_get_active (GTK_TOGGLE_ACTION (action));
+
+  if (active != GIMP_GUI_CONFIG (gimp->config)->hide_tabs)
+    g_object_set (gimp->config,
+                  "hide-tabs", active,
+                  NULL);
+}
+
+
+void
 windows_set_tabs_position_cmd_callback (GtkAction *action,
                                         GtkAction *current,
                                         gpointer   data)

app/actions/windows-commands.h
@@ -21,6 +21,10 @@
 
 void  windows_hide_docks_cmd_callback             (GtkAction *action,
                                                    gpointer   data);
+
+void  windows_hide_tabs_cmd_callback              (GtkAction *action,
+                                                   gpointer data);
+
 void  windows_set_tabs_position_cmd_callback      (GtkAction *action,
                                                    GtkAction *current,
                                                    gpointer   data);

app/config/gimpguiconfig.h
@@ -86,6 +86,7 @@ struct _GimpGuiConfig
   /* saved in sessionrc */
   gboolean             hide_docks;
   gboolean             single_window_mode;
+  gboolean             hide_tabs;
   GimpPosition         tabs_position;
   gint                 last_tip_shown;
 };

app/widgets/gimphelp-ids.h
@@ -718,6 +718,7 @@
 
 #define GIMP_HELP_WINDOWS_SHOW_DOCK               "gimp-windows-show-dock"
 #define GIMP_HELP_WINDOWS_HIDE_DOCKS              "gimp-windows-hide-docks"
+#define GIMP_HELP_WINDOWS_SHOW_TABS               "gimp-windows-hide-tabs"
 #define GIMP_HELP_WINDOWS_TABS_POSITION           "gimp-windows-tabs-position"
 #define GIMP_HELP_WINDOWS_USE_SINGLE_WINDOW_MODE  "gimp-windows-use-single-window-mode"
 #define GIMP_HELP_WINDOWS_OPEN_RECENT_DOCK        "gimp-windows-open-recent-dock"

unascribed-forks/Glimpse

app/actions/windows-actions.c
@@ -126,6 +126,13 @@ static const GimpToggleActionEntry windows_toggle_actions[] =
     FALSE,
     GIMP_HELP_WINDOWS_HIDE_DOCKS },
 
+  { "windows-show-tabs", NULL,
+    NC_("windows-action", "Show Tabs"), NULL,
+    NC_("windows-action", "When enabled, the image tabs bar is shown."),
+    G_CALLBACK (windows_show_tabs_cmd_callback),
+    FALSE,
+    GIMP_HELP_WINDOWS_SHOW_TABS },
+
   { "windows-use-single-window-mode", NULL,
     NC_("windows-action", "Single-Window Mode"), NULL,
     NC_("windows-action", "When enabled, GIMP is in a single-window mode."),
@@ -249,6 +256,7 @@ windows_actions_update (GimpActionGroup *group,
 
   SET_ACTIVE ("windows-use-single-window-mode", config->single_window_mode);
   SET_ACTIVE ("windows-hide-docks", config->hide_docks);
+  SET_ACTIVE ("windows-show-tabs", config->show_tabs);
 
   switch (config->tabs_position)
     {
@@ -271,6 +279,7 @@ windows_actions_update (GimpActionGroup *group,
 
   gimp_action_group_set_action_active (group, action, TRUE);
   gimp_action_group_set_action_sensitive (group, "windows-tab-position", config->single_window_mode);
+  gimp_action_group_set_action_sensitive (group, "windows-show-tabs", config->single_window_mode);
 
 #undef SET_ACTIVE
 }

app/actions/windows-commands.c
@@ -66,6 +66,23 @@ windows_hide_docks_cmd_callback (GtkAction *action,
 }
 
 void
+windows_show_tabs_cmd_callback (GtkAction *action,
+                                gpointer data)
+{
+  Gimp     *gimp;
+  gboolean  active;
+  return_if_no_gimp (gimp, data);
+
+  active = gtk_toggle_action_get_active (GTK_TOGGLE_ACTION (action));
+
+  if (active != GIMP_GUI_CONFIG (gimp->config)->show_tabs)
+    g_object_set (gimp->config,
+                  "show-tabs", active,
+                  NULL);
+}
+
+
+void
 windows_set_tabs_position_cmd_callback (GtkAction *action,
                                         GtkAction *current,
                                         gpointer   data)

app/actions/windows-commands.h
@@ -21,6 +21,10 @@
 
 void  windows_hide_docks_cmd_callback             (GtkAction *action,
                                                    gpointer   data);
+
+void  windows_show_tabs_cmd_callback              (GtkAction *action,
+                                                   gpointer data);
+
 void  windows_set_tabs_position_cmd_callback      (GtkAction *action,
                                                    GtkAction *current,
                                                    gpointer   data);

app/config/gimpguiconfig.h
@@ -86,6 +86,7 @@ struct _GimpGuiConfig
   /* saved in sessionrc */
   gboolean             hide_docks;
   gboolean             single_window_mode;
+  gboolean             show_tabs;
   GimpPosition         tabs_position;
   gint                 last_tip_shown;
 };

app/widgets/gimphelp-ids.h
@@ -718,6 +718,7 @@
 
 #define GIMP_HELP_WINDOWS_SHOW_DOCK               "gimp-windows-show-dock"
 #define GIMP_HELP_WINDOWS_HIDE_DOCKS              "gimp-windows-hide-docks"
+#define GIMP_HELP_WINDOWS_SHOW_TABS               "gimp-windows-show-tabs"
 #define GIMP_HELP_WINDOWS_TABS_POSITION           "gimp-windows-tabs-position"
 #define GIMP_HELP_WINDOWS_USE_SINGLE_WINDOW_MODE  "gimp-windows-use-single-window-mode"
 #define GIMP_HELP_WINDOWS_OPEN_RECENT_DOCK        "gimp-windows-open-recent-dock"
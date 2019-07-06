cocoasprite/gimp

app: move bottom-layer special casing to GimpOperationLayerMode
GimpFilter's is_last_node field only reflects the item's position
within the parent stack.  When a layer is contained in a pass-
through group, it can be the last layer of the group, while not
being the last layer in the graph as a whole (paticularly, if
there are visible layers below the group).  In fact, when we have
nested pass-through groups, whether or not a layer is the last
node depends on which group we're considering as the root (since
we exclude the backdrop from the group's projection, resulting in
different graphs for different groups).

Instead of rolling our own graph traversal, just move the relevant
logic to GimpOperationLayerMode, and let GEGL do the work for us.
At processing time, we can tell if we're the last node by checking
if we have any input.

For this to work, GimpOperationLayerMode's process() function needs
to have control over what's going on.  Replace the derived op
classes, which override process(), with a call to the layer mode's
function (as per gimp_layer_mode_get_function()) in
GimpOperationLayerMode's process() function.  (Well, actually, this
commit keeps the ops around, and just hacks around them in
gimp_layer_mode_get_operation(), because laziness :P)

Keep using the layer's is_last_node property to do the invalidation.

app/operations/layer-modes/gimp-layer-modes.h
@@ -43,6 +43,8 @@ const gchar            * gimp_layer_mode_get_operation       (GimpLayerMode     
 
 GimpLayerModeFunc        gimp_layer_mode_get_function        (GimpLayerMode        mode);
 
+GimpLayerModeAffectMask  gimp_layer_mode_get_affect_mask     (GimpLayerMode        mode);
+
 GimpLayerModeContext     gimp_layer_mode_get_context         (GimpLayerMode        mode);
 
 GimpLayerMode          * gimp_layer_mode_get_context_array   (GimpLayerMode        mode,

unascribed-forks/Glimpse

app/operations/layer-modes/gimp-layer-modes.h
@@ -62,6 +62,8 @@ const Babl               * gimp_layer_mode_get_format                 (GimpLayer
                                                                        GimpLayerColorSpace     blend_space,
                                                                        const Babl             *preferred_format);
 
+GimpLayerCompositeRegion   gimp_layer_mode_get_affected_region        (GimpLayerMode           mode);
+
 GimpLayerCompositeRegion   gimp_layer_mode_get_included_region        (GimpLayerMode           mode,
                                                                        GimpLayerCompositeMode  composite_mode);
 
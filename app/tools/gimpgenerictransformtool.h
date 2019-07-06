GNOME/gimp

app: add GimpTransformGridTool; derive most transform tools from it
While most of our transform tools use an interactive transform
grid, and have similar behavior, the flip tool is an odd one out.
The new "auto straighten" function of the measure tool introduces
another tool that performs transformations, while not behaving like
the rest of the transform tools.

Factor out the parts of GimpTransformTool that handle user
interaction into GimpTransformGridTool (with corresponding
GimpTransformGridOptions, and GimpTransformGridToolUndo), and only
leave the basic transform functionality and options in
GimpTransformTool (and GimpTransformOptions).

Derive all the transform tools (and transform-tool base classes)
that previously derived from GimpTransformTool, from
GimpTransformGridTool.  The one exception is GimpFlipTool, which
still derives from GimpTransformTool directly.  The next commit
will derive GimpMeasureTool from GimpTransformTool as well.

app/tools/gimpgenerictransformtool.h
@@ -19,7 +19,7 @@
 #define __GIMP_GENERIC_TRANSFORM_TOOL_H__
 
 
-#include "gimptransformtool.h"
+#include "gimptransformgridtool.h"
 
 
 #define GIMP_TYPE_GENERIC_TRANSFORM_TOOL            (gimp_generic_transform_tool_get_type ())
@@ -34,19 +34,19 @@ typedef struct _GimpGenericTransformToolClass GimpGenericTransformToolClass;
 
 struct _GimpGenericTransformTool
 {
-  GimpTransformTool  parent_instance;
+  GimpTransformGridTool  parent_instance;
 
-  GimpVector2        input_points[4];
-  GimpVector2        output_points[4];
+  GimpVector2            input_points[4];
+  GimpVector2            output_points[4];
 
-  GtkWidget         *matrix_grid;
-  GtkWidget         *matrix_labels[3][3];
-  GtkWidget         *invalid_label;
+  GtkWidget             *matrix_grid;
+  GtkWidget             *matrix_labels[3][3];
+  GtkWidget             *invalid_label;
 };
 
 struct _GimpGenericTransformToolClass
 {
-  GimpTransformToolClass  parent_class;
+  GimpTransformGridToolClass  parent_class;
 
   /*  virtual functions  */
   void   (* recalc_points) (GimpGenericTransformTool *generic,

unascribed-forks/Glimpse

app/tools/gimpgenerictransformtool.h
@@ -19,7 +19,7 @@
 #define __GIMP_GENERIC_TRANSFORM_TOOL_H__
 
 
-#include "gimptransformtool.h"
+#include "gimptransformgridtool.h"
 
 
 #define GIMP_TYPE_GENERIC_TRANSFORM_TOOL            (gimp_generic_transform_tool_get_type ())
@@ -34,19 +34,19 @@ typedef struct _GimpGenericTransformToolClass GimpGenericTransformToolClass;
 
 struct _GimpGenericTransformTool
 {
-  GimpTransformTool  parent_instance;
+  GimpTransformGridTool  parent_instance;
 
-  GimpVector2        input_points[4];
-  GimpVector2        output_points[4];
+  GimpVector2            input_points[4];
+  GimpVector2            output_points[4];
 
-  GtkWidget         *matrix_table;
-  GtkWidget         *matrix_labels[3][3];
-  GtkWidget         *invalid_label;
+  GtkWidget             *matrix_table;
+  GtkWidget             *matrix_labels[3][3];
+  GtkWidget             *invalid_label;
 };
 
 struct _GimpGenericTransformToolClass
 {
-  GimpTransformToolClass  parent_class;
+  GimpTransformGridToolClass  parent_class;
 
   /*  virtual functions  */
   void   (* recalc_points) (GimpGenericTransformTool *generic,
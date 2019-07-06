GNOME/gimp

Use g_set_object() in a lot of places, and splrinkle some g_clear_object()

libgimpwidgets/gimppreview.c
@@ -941,16 +941,7 @@ gimp_preview_set_default_cursor (GimpPreview *preview,
 
   priv = GET_PRIVATE (preview);
 
-  if (cursor != priv->default_cursor)
-    {
-      if (priv->default_cursor)
-        g_object_unref (priv->default_cursor);
-
-      if (cursor)
-        g_object_ref (cursor);
-
-      priv->default_cursor = cursor;
-    }
+  g_set_object (&priv->default_cursor, cursor);
 }
 
 GdkCursor *

unascribed-forks/Glimpse

This is mostly core code which we want to keep in sync with master as
long as possible, so I picked this one even though not strictly
neccessary.
(cherry picked from commit 1b7d63cce90ce6d018df9359eeb972db05b15671)

libgimpwidgets/gimppreview.c
@@ -827,13 +827,7 @@ gimp_preview_set_default_cursor (GimpPreview *preview,
 {
   g_return_if_fail (GIMP_IS_PREVIEW (preview));
 
-  if (preview->default_cursor)
-    gdk_cursor_unref (preview->default_cursor);
-
-  if (cursor)
-    gdk_cursor_ref (cursor);
-
-  preview->default_cursor = cursor;
+  g_set_object (&preview->default_cursor, cursor);
 }
 
 /**
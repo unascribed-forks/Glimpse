GNOME/gimp

Issue #701 - Add the ability to embed the GIMP built-in sRGB profile...
...upon exporting an image

Step 1: make it configurable just like "Export EXIF" etc.

app, libgimp: add "export-color-profile" config option

Add it to the preferences dialog, and pass it on to plug-ins in the
GPConfig message. Add gimp_export_color_profile() to libgimp.

Nothing uses this yet.

libgimpbase/gimpprotocol.h
@@ -26,7 +26,7 @@ G_BEGIN_DECLS
 
 /* Increment every time the protocol changes
  */
-#define GIMP_PROTOCOL_VERSION  0x0100
+#define GIMP_PROTOCOL_VERSION  0x0101
 
 
 enum
@@ -69,6 +69,7 @@ struct _GPConfig
   gint8    show_help_button;
   gint8    use_cpu_accel;
   gint8    use_opencl;
+  gint8    export_profile;
   gint8    export_exif;
   gint8    export_xmp;
   gint8    export_iptc;

unascribed-forks/Glimpse

(cherry picked from commit 8c9c09102135ca93a0da641f70f411149d10cedc)

libgimpbase/gimpprotocol.h
@@ -26,7 +26,7 @@ G_BEGIN_DECLS
 
 /* Increment every time the protocol changes
  */
-#define GIMP_PROTOCOL_VERSION  0x0017
+#define GIMP_PROTOCOL_VERSION  0x0018
 
 
 enum
@@ -73,7 +73,7 @@ struct _GPConfig
   gint8    export_exif;
   gint8    export_xmp;
   gint8    export_iptc;
-  gint8    install_cmap;
+  gint8    export_profile;
   gint8    show_tooltips;
   gint32   min_colors;
   gint32   gdisp_ID;